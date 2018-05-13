db5ca21ca028ebcc65c1b11f1721f0b69ddc9b97
Yosuke <parmalatinter@gmail.com>
2018-05-14 02:26:31 +0900

    管理者seeder更新

--- seeds/admin_users_seed.js
+++ seeds/admin_users_seed.js
@@ -5,18 +5,22 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('admin_users').insert([
           {
-              "id" : "ktoshi515226@yahoo.co.jp",
+              "id" : 1,
+              "email" : "ktoshi515226@yahoo.co.jp",
               "password" : "worksap",
               "name" : "としのり",
+              "send_message" : "こんにちわ",
               "memo" : "最初のユーザー男性",
               "sex" : 1,
               "is_enable" : 1,
               'created_at' : knex.fn.now()
           },
           {
-              "id" : "k.toshi5152261030@gmail.com",
+              "id" : 2,
+              "email" : "k.toshi5152261030@gmail.com",
               "password" : "worksap",
               "name" : "makiko51",
+              "send_message" : "こんにちわ",
               "memo" : "最初のユーザー女性",
               "sex" : 2,
               "is_enable" : 1,
