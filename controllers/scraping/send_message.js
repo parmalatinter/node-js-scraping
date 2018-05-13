ea60b654d2677a1740968619c2af9ce81f495665
Yosuke <parmalatinter@gmail.com>
2018-05-14 02:09:03 +0900

    メッセージ送信用コントローラー追加　更新

--- controllers/scraping/send_message.js
+++ controllers/scraping/send_message.js
@@ -1,43 +1,53 @@
-exports.exec = function (host_res, knex, puppeteer) {
-    const admin_user = require('./../../models/admin/user');
-    const user = require('./../../models/user/user');
-    const send_message = require('./../../libs/puppeteer/send_message');
-    const async = require('async');
-    admin_user.get_list(knex).then(function (user_records) {
-        let sent_count = 0;
-        async.each(user_records, function (user_record, callback) {
-            let target_sex = user_record.sex === 1 ? 2 : 1;
-            user.get_enable_send_list(knex, target_sex).then(function (send_list) {
-                if (!send_list.length) {
-                    callback();
-                    return;
-                }
-                send_message.exec(puppeteer, user_record, send_list).then(function (sent_list) {
-                    async.each(sent_list, function (sent_obj, callback2) {
-                        user.update_sent(knex, sent_obj.id).then(function () {
-                            sent_count++;
-                            callback2();
+exports.exec = function (host_res, knex, puppeteer, setting_row, env) {
+    return new Promise(function (resolve, reject) {
+        const admin_user = require('./../../models/admin/user');
+        const user = require('./../../models/user/user');
+        const send_message = require('./../../libs/puppeteer/send_message');
+        const async = require('async');
+        const message_send_status = require('./../../models/status/message_send_status');
+        let running_type = message_send_status.running_type_configs.running;
+        admin_user.get_enable_list(knex).then(function (user_records) {
+            let sent_count = 0;
+            async.each(user_records, function (user_record, callback) {
+                let target_sex = user_record.sex === 1 ? 2 : 1;
+                user.get_enable_send_list(knex, target_sex).then(function (send_list) {
+                    if (!send_list.length) {
+                        callback(true);
+                        return;
+                    }
+                    send_message.exec(puppeteer, knex, user_record, send_list, setting_row, env).then(function (sent_list) {
+                        if (!sent_list.length) callback(false);
+                        async.each(sent_list, function (sent_obj, callback2) {
+                            user.update_sent(knex, sent_obj.id).then(function () {
+                                sent_count++;
+                                callback2();
+                            }, function (e) {
+                                throw e;
+                            });
                         }, function (e) {
-                            throw e;
+                            if (e) {
+                                throw e;
+                            }
+                            callback();
                         });
-                    }, function (e) {
-                        if (e) {
-                            throw e;
-                        }
-                        callback();
+                    }).catch(function (e) {
+                        throw e;
                     });
                 });
+
+            }, function (e) {
+                console.log('message_send finished');
+                if (e) {
+                    running_type = message_send_status.running_type_configs.stopping;
+                    message_send_status.update_running_type(knex, running_type);
+                    reject(e);
+                }
+                resolve(true);
             });
-
-        }, function (e) {
-            if (e) {
-                host_res.render('pages/error', {error: e});
-            }
-            host_res.render('pages/message_sent_result', {sent_count: sent_count});
-        });
-    }, function (e) {
-        host_res.render('pages/error', {error: e});
-    })
-
-
+        }).catch(function (e) {
+            running_type = message_send_status.running_type_configs.stopping;
+            message_send_status.update_running_type(knex, running_type);
+            host_res.render('pages/error', {error: e});
+        })
+    });
 };
