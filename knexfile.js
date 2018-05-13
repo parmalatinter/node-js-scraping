5c79011d18f06dee7a58b57cb6ddbbccdef7f249
Yosuke <parmalatinter@gmail.com>
2018-05-14 02:18:41 +0900

    db設定ファイル更新

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,10 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'postgresql',
     connection: {
-      database: 'scraping',
+      database: 'scraping-dev',
       user:     'postgres',
       password: 'postgres',
       host: 'localhost'
@@ -17,36 +16,19 @@ module.exports = {
     debug: true
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      database: 'scraping-pro',
+      user:     'postgres',
+      password: 'postgres',
+      host: 'localhost'
     },
     migrations: {
+      directory:'./db/migrations',
       tableName: 'knex_migrations'
-    }
+    },
+    debug: false
   }
 
 };
