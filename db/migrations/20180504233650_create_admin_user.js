aca5150ccc1e2f7824a6421d519376538800d374
Yosuke <parmalatinter@gmail.com>
2018-05-14 02:16:09 +0900

    管理ユーザーマイグレーション更新

--- db/migrations/20180504233650_create_admin_user.js
+++ db/migrations/20180504233650_create_admin_user.js
@@ -2,15 +2,19 @@ exports.up = function (knex, Promise) {
     return knex.schema.hasTable("admin_users").then(function (exists) {
         if (!exists) {
             knex.schema.createTable("admin_users", function (t) {
-                t.string("id").primary();
-                t.string("name", 20);
+                t.increments("id").primary();
+                t.string("email", 30);
+                t.string("name", 20).defaultTo('');
                 t.string("password", 20);
-                t.string("memo");
-                t.integer("sex", 1);
-                t.boolean("is_enable", true);
-                t.timestamp("created_at");
+                t.string("send_message").defaultTo('');
+                t.string("memo").defaultTo('');
+                t.integer("sex", 1).defaultTo(0);
+                t.boolean("is_enable", true).defaultTo(true);
+                t.timestamp("created_at").defaultTo(knex.fn.now());
+                t.index('email');
                 t.index('name');
                 t.index('password');
+                t.index('send_message');
                 t.index('memo');
                 t.index('sex');
                 t.index('is_enable');
