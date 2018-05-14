ca40256ca5770dcaac1dbd753f9b05722c633227
Yosuke <parmalatinter@gmail.com>
2018-05-15 00:52:57 +0900

    dbコントローラーファイルをまとめた

---
+++ controllers/db/db.js
@@ -0,0 +1,68 @@
+const session_message = require('./../../libs/express/session_message');
+const output_error = require('./../error/error');
+
+exports.backup = function (req, host_res) {
+    const db = require('../../libs/db/db');
+    db.dump(req).then(function (result) {
+        session_message.set_message(req, 'バックアップしました。');
+        host_res.redirect('/admin/db');
+    }, function (e) {
+        req.app.locals.render(req, host_res, 'pages/error', {error: e});
+    });
+};
+
+exports.backup_list = function (req, host_res) {
+    const dir = './seeds/dump';
+    const fs = require('fs');
+    const moment = require('moment');
+
+    let backup_list = [];
+    fs.readdir(dir, (err, files) => {
+        for(let file_name of files){
+            const timestamp = file_name.replace('dump.', '');
+            const date_str = moment.unix(timestamp).format("YYYY年MM月DD日 HH:mm:ssZ");
+            backup_list.push({date_str: date_str, file_name : file_name});
+        }
+        req.app.locals.render(req, host_res, 'pages/admin/db', {backup_list: backup_list.reverse()});
+    });
+};
+
+exports.delete_backup = function (req, host_res) {
+    const db = require('../../libs/db/db');
+    db.delete(req).then(function (result) {
+        session_message.set_message(req, '削除しました。');
+        host_res.redirect('/admin/db');
+    }, function (e) {
+        req.app.locals.render(req, host_res, 'pages/error', {error: e});
+    });
+};
+
+exports.reset = function (knex, req, host_res, env) {
+    const db = require('./../../libs/db/db');
+    db.drop(knex).then(function () {
+        db.create(env).then(function () {
+            db.init(env).then(function () {
+                session_message.set_message(req, 'リセットしました。');
+                host_res.redirect('/admin/db');
+            }).catch(function (error) {
+                output_error.exec(host_res, error, '初期化に失敗しました');
+            });
+        }).catch(function (error) {
+            output_error.exec(host_res, error, 'DB作成に失敗しました');
+        });
+    }).catch(function (error) {
+        output_error.exec(host_res, error, 'DB削除に失敗しました');
+    }, function (e) {
+        host_res.render('pages/error', {error: e});
+    });
+};
+
+exports.restore = function (req, host_res) {
+    const db = require('../../libs/db/db');
+    db.restore(req).then(function (result) {
+        session_message.set_message(req, 'レストアしました。');
+        host_res.redirect('/admin/db');
+    }, function (e) {
+        req.app.locals.render(req, host_res, 'pages/error', {error: e});
+    });
+};
--- index.js
+++ index.js
@@ -42,111 +42,100 @@ db_status.get(knex).then(function (res) {
             req.app.locals.render(req, host_res, 'pages/index', {contents: [], home_url: '', name: 'test'});
         })
         .post('/admin/user', function (req, host_res) {
-            let users = require('./controllers/admin/users');
+            const users = require('./controllers/admin/users');
             users.exec(req, host_res, knex);
         })
         .get('/admin/user', function (req, host_res) {
-            let users = require('./controllers/admin/users');
+            const users = require('./controllers/admin/users');
             users.exec(req, host_res, knex);
         })
         .post('/admin/user/set_is_enable_send', function (req, host_res) {
-            let users = require('./controllers/admin/users');
+            const users = require('./controllers/admin/users');
             users.set_is_enable_send(req, host_res, knex, true);
         })
         .post('/admin/user/unset_is_enable_send', function (req, host_res) {
-            let users = require('./controllers/admin/users');
+            const users = require('./controllers/admin/users');
             users.set_is_enable_send(req, host_res, knex, false);
         })
         .post('/admin/user/set_is_enable_send_by_ids', function (req, host_res) {
-            let users = require('./controllers/admin/users');
+            const users = require('./controllers/admin/users');
             users.set_is_enable_send_by_ids(req, host_res, knex);
         })
         .get('/admin/admin_user', function (req, host_res) {
-            let users = require('./controllers/admin/admin_users');
+            const users = require('./controllers/admin/admin_users');
             users.exec(req, host_res, knex, null);
         })
         .post('/admin/admin_user/insert', function (req, host_res) {
-            let users = require('./controllers/admin/admin_users');
+            const users = require('./controllers/admin/admin_users');
             users.insert(req, host_res, knex, null);
         })
         .post('/admin/admin_user/delete', function (req, host_res) {
-            let users = require('./controllers/admin/admin_users');
+            const users = require('./controllers/admin/admin_users');
             users.delete(req, host_res, knex, null);
         })
         .post('/admin/admin_user/update', function (req, host_res) {
-            let users = require('./controllers/admin/admin_users');
+            const users = require('./controllers/admin/admin_users');
             users.update(req, host_res, knex, null);
         })
         .get('/admin/setting', function (req, host_res) {
-            let setting = require('./controllers/admin/setting');
+            const setting = require('./controllers/admin/setting');
             setting.exec(req, host_res, knex, null);
         })
         .post('/admin/setting/update', function (req, host_res) {
-            let setting = require('./controllers/admin/setting');
+            const setting = require('./controllers/admin/setting');
             setting.update(req, host_res, knex);
         })
         .get('/admin/db', function (req, host_res) {
-            let backup_list = require('./controllers/db/backup_list');
-            backup_list.exec(req, host_res);
+            const db = require('./controllers/db/db');
+            db.backup_list(req, host_res);
         })
         .get('/admin/db/backup', function (req, host_res) {
-            let backup = require('./controllers/db/backup');
-            backup.exec(req, host_res);
+            const db = require('./controllers/db/db');
+            db.backup(req, host_res);
         })
         .get('/admin/db/restore', function (req, host_res) {
-            let restore = require('./controllers/db/restore');
-            restore.exec(req, host_res);
+            const db = require('./controllers/db/db');
+            db.restore(req, host_res);
         })
         .get('/admin/db/delete_backup', function (req, host_res) {
-            let delete_backup = require('./controllers/db/delete');
-            delete_backup.exec(req, host_res);
+            const db = require('./controllers/db/db');
+            db.delete_backup(req, host_res);
         })
         .get('/admin/db/reset', function (req, host_res) {
-            let reset = require('./controllers/db/reset');
-            reset.exec(knex, req, host_res, env);
+            const db = require('./controllers/db/db');
+            db.reset(knex, req, host_res, env);
         })
         .post('/ajax/get_status', function (req, host_res) {
-            let status = require('./controllers/ajax/status/status');
+            const status = require('./controllers/ajax/status/status');
             status.get_running_type(knex, host_res);
         })
         .post('/ajax/user/get_info_all_count', function (req, host_res) {
-            let user = require('./controllers/ajax/user/user');
+            const user = require('./controllers/ajax/user/user');
             user.get_info_all_count(knex, host_res);
         })
         .post('/ajax/user/get_send_all_count', function (req, host_res) {
-            let user = require('./controllers/ajax/user/user');
+            const user = require('./controllers/ajax/user/user');
             user.get_send_all_count(knex, host_res);
         })
         .get('/scraping/start', function (req, host_res) {
-            let users_info = require('./controllers/scraping/users_info');
+            const users_info = require('./controllers/scraping/users_info');
             setting.get(knex).then(function (setting_row) {
                 users_info.exec(host_res, knex, puppeteer, setting_row, env);
             })
         })
         .get('/scraping/stop', function (req, host_res) {
-            let status = require('./controllers/ajax/status/status');
+            const status = require('./controllers/ajax/status/status');
             status.stop_info_running(knex, host_res);
         })
         .get('/message/send', function (req, host_res) {
-            let send_message = require('./controllers/scraping/send_message');
-            send_message.start(host_res, knex, puppeteer, setting_row, env).catch(err => {
-                console.log(err);
-            });
+            const send_message = require('./controllers/scraping/send_message');
+            send_message.exec(req, host_res, knex, puppeteer, env);
         })
         .get('/message/stop', function (req, host_res) {
-            let status = require('./controllers/ajax/status/status');
+            const status = require('./controllers/ajax/status/status');
             status.stop_send_running(knex, host_res);
         })
-        .get('/message/send_by_frequency', function (req, host_res) {
-            let send_message = require('./controllers/scraping/send_message');
-            setting.get_frequency_minute(knex).then(function (minute) {
-                setInterval(function () {
-                    setting.is_headless_mode(knex).then(function (is_headless_mode) {
-                        send_message.exec(host_res, knex, puppeteer, is_headless_mode);
-                    });
-                }, minute * 60)
-            })
-        }).listen(PORT, () => console.log(`Listening on ${ PORT }`));
+        .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 }).catch(function (res) {
     if (res.err) {
         const shell = require("./controllers/shell/init");
