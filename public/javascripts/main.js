e1f935638bc5c337dbfcd6f75f9f4ce13c5ba683
Yosuke <parmalatinter@gmail.com>
2018-05-16 20:07:46 +0900

    リファファクタリング

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,10 +9,12 @@ $(function () {
 
 var show_modal_message = function (title) {
     $('#modal_message .modal-title').text(title);
+    $('#modal_message').modal('show');
 };
 
 var show_modal_error = function (title, error, stack) {
     $('#modal_message .modal-title').text(title);
     $('#modal_message .modal-error').text(error);
     $('#modal_message .modal-stack').text(stack);
+    $('#modal_error').modal('show');
 };
